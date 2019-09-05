ed38a6c1791344467e5f19ebec65d9b72019d90c
John Elder <john@codemy.com>
2019-09-05 10:51:25 -0700

    initial commit

---
+++ lookup/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LookupConfig(AppConfig):
+    name = 'lookup'
---
+++ lookup/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def home(request):
+	return render(request, 'home.html', {})
+
+
+def about(request):
+	return render(request, 'about.html', {})
